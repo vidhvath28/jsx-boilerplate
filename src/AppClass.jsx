443f7ab8e42ed4553aa8c5e3406c108772dcc350
vidhvath28 <138788842+vidhvath28@users.noreply.github.com>
2023-11-02 16:00:46 +0530

    Updates

--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -25,6 +25,27 @@ export default class AppClass extends Component{
     ]
     return data;
   }
+  render(){
 
-  // code here
+    const imgData = this.imageData();
+    return (
+      <div>
+        <h1>Kalvium gallary</h1>
+        <div className="row">
+        {
+         imgData.map((image) => {
+          return(
+            <div className="column"key={image.id}>
+              <img src={image.img} alt=""/>
+            </div>
+          )
+          
+          })};
+      </div>
+  
+      </div>
+    );
+        
+}
+
 }
